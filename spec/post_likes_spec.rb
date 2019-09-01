a74ed1b7ba93dbbd4d1593f7fe908bf39dcfe03c
Will Howes <williameldenhowes@gmail.com>
2019-09-01 15:47:24 +0100

    Refactored

--- spec/post_likes_spec.rb
+++ spec/post_likes_spec.rb
@@ -1,32 +1,30 @@
 require 'post_liked_by'
 
-# likes ["Peter"] // must be "Peter likes this"
 describe '.post_liked_by' do
   it 'for one like, confirms that person likes the post' do
     expect(post_liked_by(["Peter"])).to eq("Peter likes this")
   end
 
   it 'confirms for two people liking post' do
-    # likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
     expect(post_liked_by(["Jacob", "Alex"])).to eq("Jacob and Alex like this")
   end
 
   it 'confirms for three people liking post' do
-    expect(post_liked_by(["Max", "John", "Mark"])).to eq("Max, John and "\
-                                                          "Mark like this")
+    expect(post_liked_by(["Max",
+                          "John",
+                          "Mark"])).to eq("Max, John and Mark like this")
   end
 
-  # likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
   it "confirms for four people liking post" do
-    expect(post_liked_by(["Alex", "Jacob", "Mark", "Max"])).to eq("Alex, Jacob"\
-                                                                  " and 2 others"\
-                                                                  " like this")
+    expect(post_liked_by(["Alex", "Jacob",
+                          "Mark", "Max"])).to eq("Alex, Jacob and 2 others"\
+                                                  " like this")
   end
 
   it 'confirms for more than four people liking post' do
-    expect(post_liked_by(["Alex", "Jacob", "Mark", "Max", "Will"])).to eq("Alex,"\
-                                                                          " Jacob"\
-                                                                          " and 3 others like this")
+    expect(post_liked_by(["Alex", "Jacob",
+                          "Mark", "Max",
+                          "Will"])).to eq("Alex, Jacob and 3 others like this")
   end
 
   it 'confirms no one likes the post if input is empty' do
