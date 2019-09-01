bc6a5cc0d082b07c73399ea8ae230f9ef898fb35
Will Howes <williameldenhowes@gmail.com>
2019-09-01 15:35:55 +0100

    Test for four names passes

--- lib/post_liked_by.rb
+++ lib/post_liked_by.rb
@@ -3,9 +3,9 @@ def post_liked_by(names)
     "#{names[0]} likes this"
   elsif names.length == 2
     "#{names[0]} and #{names[1]} like this"
-  else
+  elsif names.length == 3
     "#{names[0]}, #{names[1]} and #{names[2]} like this"
-
-
+  else
+    "#{names[0]}, #{names[1]} and #{names.length - 2} others like this"
   end
 end
--- spec/post_likes_spec.rb
+++ spec/post_likes_spec.rb
@@ -14,4 +14,9 @@ describe '.post_liked_by' do
   it 'confirms for three people liking post' do
     expect(post_liked_by(["Max", "John", "Mark"])).to eq("Max, John and Mark like this")
   end
+
+  # likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+  it "confirms for four people liking post" do
+    expect(post_liked_by(["Alex", "Jacob", "Mark", "Max"])).to eq("Alex, Jacob and 2 others like this")
+  end
 end
