226a9a04580153cc42072762352ba24ea90b2276
Will Howes <williameldenhowes@gmail.com>
2019-09-01 15:37:24 +0100

    Refactored

--- lib/post_liked_by.rb
+++ lib/post_liked_by.rb
@@ -1,10 +1,16 @@
 def post_liked_by(names)
-  if names.length == 1
+
+  case names.length
+
+  when 1
     "#{names[0]} likes this"
-  elsif names.length == 2
+
+  when 2
     "#{names[0]} and #{names[1]} like this"
-  elsif names.length == 3
+
+  when 3
     "#{names[0]}, #{names[1]} and #{names[2]} like this"
+
   else
     "#{names[0]}, #{names[1]} and #{names.length - 2} others like this"
   end
